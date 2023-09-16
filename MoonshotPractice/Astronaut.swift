fad53c92ee69c50ff6f4affc2bbce0fba1791ac1
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-09-16 16:05:07 -0400

    Practiced building Moonshot again

---
+++ MoonshotPractice/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  MoonshotPractice
+//
+//  Created by Jordan Haynes on 9/16/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
+
---
+++ MoonshotPractice/Color-Theme.swift
@@ -0,0 +1,20 @@
+//
+//  Color-Theme.swift
+//  MoonshotPractice
+//
+//  Created by Jordan Haynes on 9/16/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    static var darkBackground: Color {
+        Color(red: 0.1, green: 0.1, blue: 0.2)
+    }
+
+    static var lightBackground: Color {
+        Color(red: 0.2, green: 0.2, blue: 0.3)
+    }
+}
+

bc7934115eff895c9993e4a083828095b10c7db7
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-09-16 15:14:38 -0400

    Initial Commit

---
+++ MoonshotPractice/MoonshotPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotPracticeApp.swift
+//  MoonshotPractice
+//
+//  Created by Jordan Haynes on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
